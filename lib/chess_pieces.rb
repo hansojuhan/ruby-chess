f03256294c26fea56a2b9d760c2e21eacf121a3f
hje <hjerikson@gmail.com>
2024-05-09 21:37:35 +0200

    Implement 3/5 pawn valid moves

--- lib/chess_pieces.rb
+++ lib/chess_pieces.rb
@@ -33,15 +33,36 @@ end
 class Pawn < ChessPiece
   # Check if pawn can make this move
   def valid_move?(board, origin, destination)
-    # Destination should be empty
-    return false unless board[destination[0]][destination[1]].nil?
-    
     # Valid moves
     # 1. One square forward (white: decrement row, black: increment row)
-    return true if destination[0] == origin[0] - 1
+    if 
+      destination[0] == origin[0] - 1 && 
+      destination[1] == origin[1] &&
+      board[destination[0]][destination[1]].nil?
+
+      return true
+    end
 
     # 2. Two squares forward, if first move
-    return true if destination[0] == origin[0] - 2 && moves_done == 0
+    if 
+      destination[0] == origin[0] - 2 && 
+      destination[1] == origin[1] && 
+      moves_done == 0 && 
+      board[destination[0]][destination[1]].nil?
+
+      return true
+    end
+
+    # 3. Take: enemy piece is diagonal
+    if 
+      destination[0] == origin[0] - 1 && 
+      (destination[1] == origin[1] - 1 || destination[1] == origin[1] + 1) &&
+      (!board[destination[0]][destination[1]].nil? && board[destination[0]][destination[1]].color != self.color)
+
+      return true
+    end
+
+    return false
   end
 end
 
